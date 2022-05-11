995c34bee46112df778d84892edfd5f0d22e7a20
max397574 <max.heinzer397@gmx.ch>
2022-05-11 21:14:58 +0200

    feat(utils): added jeskape

---
+++ lua/omega/modules/utils/insert_utils.lua
@@ -0,0 +1,24 @@
+local insert = {}
+
+insert.plugins = {
+    ["jeskape.nvim"] = { "Krafi2/jeskape.nvim", event = "InsertEnter" },
+}
+
+insert.configs = {
+    ["jeskape.nvim"] = function()
+        require("jeskape").setup({
+            mappings = {
+                [","] = {
+                    [","] = "<cmd>lua require'omega.utils'.append_comma()<CR>",
+                },
+                j = {
+                    k = "<esc>",
+                    [","] = "<cmd>lua require'omega.utils'.append_comma()<CR><esc>o",
+                    j = "<esc>A<cr>",
+                },
+            },
+        })
+    end,
+}
+
+return insert
