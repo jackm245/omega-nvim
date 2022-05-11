e868cdc2ae21697c8bc19f415049634658f42d06
max397574 <max.heinzer397@gmx.ch>
2022-05-11 21:14:19 +0200

    feat(utils): added `append_comma`

--- lua/omega/utils/init.lua
+++ lua/omega/utils/init.lua
@@ -37,4 +37,14 @@ function utils.bootstrap_impatient()
     end
 end
 
+---Appends a `,` to the current line
+function utils.append_comma()
+    -- save cursor position
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    -- append ,
+    vim.cmd([[normal A,]])
+    -- restore cursor position
+    vim.api.nvim_win_set_cursor(0, cursor)
+end
+
 return utils
