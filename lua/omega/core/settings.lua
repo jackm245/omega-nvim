6c9bff909e66f81e0095951567ede78cd6e1cbe8
max397574 <max.heinzer397@gmx.ch>
2022-05-11 21:52:27 +0200

    feat: moved settings and disable builtin

--- lua/omega/core/settings.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.shiftwidth = 4
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.termguicolors = true
-vim.opt.mouse = "a"
---
+++ lua/omega/core/settings/init.lua
@@ -0,0 +1,12 @@
+-- disable builtin plugins
+require"omega.core.settings.disable_builtin"
+
+-- use lua filetype detection
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
+
+vim.opt.shiftwidth = 4
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
