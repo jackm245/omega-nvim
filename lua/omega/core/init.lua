aa38c6c64c57d9a52764256b013aae787f4f2f80
max397574 <max.heinzer397@gmx.ch>
2022-05-11 22:08:45 +0200

    feat(core): added mappings and autocommands

--- lua/omega/core/init.lua
+++ lua/omega/core/init.lua
@@ -1,5 +1,7 @@
 local utils = require("omega.utils")
 local modules = require("omega.core.modules")
+-- set early so mappings are defined with it
+vim.g.mapleader = " "
 
 -- create `omega` gloabel variable
 require("omega.core.omega_global")
@@ -9,6 +11,8 @@ modules.bootstrap_packer()
 utils.bootstrap_impatient()
 
 require("omega.core.settings")
+require("omega.core.autocommands")
+require("omega.core.mappings")
 
 require("omega.core.ui")
 modules.setup()
