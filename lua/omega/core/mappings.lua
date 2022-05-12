82d307b777851d8d6c8158dc1dd514c23b5d9b3a
max397574 <max.heinzer397@gmx.ch>
2022-05-12 11:48:06 +0200

    feat(mappings): added `insert`

--- lua/omega/core/mappings.lua
+++ lua/omega/core/mappings.lua
@@ -1,6 +1,17 @@
-vim.keymap.set(
-    "n",
-    "<leader>y",
-    '"+y',
-    { noremap = true, desc = " Yank to clipboard" }
-)
+local wk = require("which-key")
+wk.register({
+    y = { '"+y', " Yank to clipboard" },
+    ["S"] = { "<cmd>w<cr>", " Save" },
+    i = {
+        name = " Insert",
+        o = { "o<ESC>k", "Empty line below" },
+        O = { "O<ESC>j", "Empty line above" },
+        i = { "i <ESC>l", "Space before" },
+        a = { "a <ESC>h", "Space after" },
+        e = { "<cmd>Telescope emoji<cr>", "Emoji" },
+        ["<CR>"] = { "i<CR><ESC>", "Linebreak at Cursor" },
+    },
+}, {
+    prefix = "<leader>",
+    mode = "n",
+})
