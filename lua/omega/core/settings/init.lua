ec5cd016f4cd7e916c517b967dd64e0098369559
max397574 <max.heinzer397@gmx.ch>
2022-05-12 11:11:16 +0200

    feat(settings): added timeoutlen

--- lua/omega/core/settings/init.lua
+++ lua/omega/core/settings/init.lua
@@ -5,6 +5,7 @@ require("omega.core.settings.disable_builtin")
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
 
+vim.opt.timeoutlen = 300
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
 vim.opt.number = true
