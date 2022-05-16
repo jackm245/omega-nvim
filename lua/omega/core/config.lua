205875d99be4e3b3a0b950b7ff92bb6cd20c2c58
max397574 <max.heinzer397@gmx.ch>
2022-05-16 13:18:33 +0200

    feat(config): added colorscheme

--- lua/omega/core/config.lua
+++ lua/omega/core/config.lua
@@ -3,6 +3,8 @@ local config = {}
 function config.load()
     omega.config = {
         statusline = "round",
+        colorscheme = "doom_one",
+        light_colorscheme = "everforest",
     }
 end
 
