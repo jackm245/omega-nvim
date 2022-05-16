25281142d3d359a8ece66303b677740974bfd6ef
max397574 <max.heinzer397@gmx.ch>
2022-05-16 13:20:40 +0200

    feat(completion): add neorgen keybind

--- lua/omega/modules/completion/annotations.lua
+++ lua/omega/modules/completion/annotations.lua
@@ -1,19 +1,36 @@
+---@type OmegaModule
 local neogen_mod = {}
 
 neogen_mod.plugins = {
     ["neogen"] = {
         "danymat/neogen",
         module = { "neogen" },
+        requires = { "LuaSnip" },
     },
 }
 
-neogen_mod.configs={
-    ["neogen"]=function()
-            require("neogen").setup({
-                snippet_engine = "luasnip",
-                enabled = true,
-            })
-    end
+neogen_mod.configs = {
+    ["neogen"] = function()
+        require("neogen").setup({
+            snippet_engine = "luasnip",
+            enabled = true,
+        })
+    end,
 }
+
+neogen_mod.keybindings = function()
+    local wk = require("which-key")
+    wk.register({
+        a = {
+            function()
+                require("neogen").generate({ snippet_engine = "luasnip" })
+            end,
+            "﨧Annotations",
+        },
+    }, {
+        prefix = "<leader>",
+        mode = "n",
+    })
+end
 
 return neogen_mod
