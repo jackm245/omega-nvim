83114f866a7929ae14b613c8e5c9a0ab9e0b173d
max397574 <max.heinzer397@gmx.ch>
2022-05-16 13:19:09 +0200

    feat(utils): added types

---
+++ lua/omega/utils/types.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: unused-local
+--# selene: allow(unused_variable)
+
+---A module for omega config
+---@class OmegaModule
+---@field keybindings function Set up keybindings for this module
+---@field plugins table Keys
+---@field configs table
