48a522a81a07d506b2987173a873f9e2326981fd
max397574 <max.heinzer397@gmx.ch>
2022-05-16 15:43:09 +0200

    chore: autoformat with stylua

--- lua/omega/modules/tools/telescope.lua
+++ lua/omega/modules/tools/telescope.lua
@@ -32,9 +32,9 @@ tele_mod.plugins = {
         setup = function()
             vim.ui.select = function(items, opts, on_choice)
                 vim.cmd([[
-		    PackerLoad telescope.nvim
-		    PackerLoad telescope-ui-select.nvim
-		]])
+                    PackerLoad telescope.nvim
+                    PackerLoad telescope-ui-select.nvim
+                ]])
                 require("telescope").load_extension("ui-select")
                 vim.ui.select(items, opts, on_choice)
             end
@@ -59,11 +59,11 @@ tele_mod.configs = {
 
         local calc_tabline = function(max_lines)
             local tbln = (vim.o.showtabline == 2)
-                or (
-                    vim.o.showtabline == 1
-                    and #vim.api.nvim_list_tabpages()
-                        > 1
-                )
+            or (
+            vim.o.showtabline == 1
+            and #vim.api.nvim_list_tabpages()
+            > 1
+        )
             if tbln then
                 max_lines = max_lines - 1
             end
@@ -79,224 +79,224 @@ tele_mod.configs = {
         end
 
         local calc_size_and_spacing =
-            function(cur_size, max_size, bs, w_num, b_num, s_num)
-                local spacing = s_num * (1 - bs) + b_num * bs
-                cur_size = math.min(cur_size, max_size)
-                cur_size = math.max(cur_size, w_num + spacing)
-                return cur_size, spacing
-            end
+        function(cur_size, max_size, bs, w_num, b_num, s_num)
+            local spacing = s_num * (1 - bs) + b_num * bs
+            cur_size = math.min(cur_size, max_size)
+            cur_size = math.max(cur_size, w_num + spacing)
+            return cur_size, spacing
+        end
 
         require("telescope.pickers.layout_strategies").custom_bottom =
-            function(self, max_columns, max_lines, layout_config)
-                local initial_options = p_window.get_initial_window_options(
-                    self
-                )
-                local results = initial_options.results
-                local prompt = initial_options.prompt
-                local preview = initial_options.preview
-                layout_config = {
-                    anchor = "S",
-                    -- preview_width = 0.6,
-                    bottom_pane = {
-                        height = 0.5,
-                        preview_cutoff = 20,
-                        prompt_position = "top",
-                    },
-                    custom_bottom = {
-                        height = 0.5,
-                        preview_cutoff = 20,
-                        -- preview_width = 0.6,
-                        prompt_position = "top",
-                    },
-                    center = {
-                        height = 0.5,
-                        preview_cutoff = 20,
-                        prompt_position = "top",
-                        width = 0.99,
-                    },
-                    cursor = {
-                        height = 0.5,
-                        preview_cutoff = 20,
-                        width = 0.99,
-                    },
-                    flex = {
-                        horizontal = {},
-                        preview_width = 0.65,
-                    },
+        function(self, max_columns, max_lines, layout_config)
+            local initial_options = p_window.get_initial_window_options(
+                self
+            )
+            local results = initial_options.results
+            local prompt = initial_options.prompt
+            local preview = initial_options.preview
+            layout_config = {
+                anchor = "S",
+                -- preview_width = 0.6,
+                bottom_pane = {
                     height = 0.5,
-                    horizontal = {
-                        height = 0.5,
-                        preview_cutoff = 20,
-                        preview_width = 0.65,
-                        prompt_position = "top",
-                        width = 0.99,
-                    },
                     preview_cutoff = 20,
                     prompt_position = "top",
-                    vertical = {
-                        height = 0.95,
-                        preview_cutoff = 20,
-                        preview_height = 0.5,
-                        preview_width = 0.65,
-                        prompt_position = "top",
-                        width = 0.9,
-                    },
+                },
+                custom_bottom = {
+                    height = 0.5,
+                    preview_cutoff = 20,
+                    -- preview_width = 0.6,
+                    prompt_position = "top",
+                },
+                center = {
+                    height = 0.5,
+                    preview_cutoff = 20,
+                    prompt_position = "top",
                     width = 0.99,
-                }
+                },
+                cursor = {
+                    height = 0.5,
+                    preview_cutoff = 20,
+                    width = 0.99,
+                },
+                flex = {
+                    horizontal = {},
+                    preview_width = 0.65,
+                },
+                height = 0.5,
+                horizontal = {
+                    height = 0.5,
+                    preview_cutoff = 20,
+                    preview_width = 0.65,
+                    prompt_position = "top",
+                    width = 0.99,
+                },
+                preview_cutoff = 20,
+                prompt_position = "top",
+                vertical = {
+                    height = 0.95,
+                    preview_cutoff = 20,
+                    preview_height = 0.5,
+                    preview_width = 0.65,
+                    prompt_position = "top",
+                    width = 0.9,
+                },
+                width = 0.99,
+            }
 
-                local tbln
-                max_lines, tbln = calc_tabline(max_lines)
+            local tbln
+            max_lines, tbln = calc_tabline(max_lines)
 
-                local height = if_nil(
-                    resolve.resolve_height(layout_config.height)(
-                        self,
-                        max_columns,
-                        max_lines
-                    ),
-                    25
-                )
-                local width_opt = layout_config.width
-
-                local width = resolve.resolve_width(width_opt)(
+            local height = if_nil(
+                resolve.resolve_height(layout_config.height)(
                     self,
                     max_columns,
                     max_lines
-                )
+                ),
+                25
+            )
+            local width_opt = layout_config.width
 
-                -- local height = 21
-                if
-                    type(layout_config.height)
-                        == "table"
-                    and type(layout_config.height.padding) == "number"
-                then
-                    height = math.floor((max_lines + height) / 2)
-                end
-                prompt.border = results.border
+            local width = resolve.resolve_width(width_opt)(
+                self,
+                max_columns,
+                max_lines
+            )
 
-                local bs = get_border_size(self)
+            -- local height = 21
+            if
+                type(layout_config.height)
+                == "table"
+                and type(layout_config.height.padding) == "number"
+            then
+                height = math.floor((max_lines + height) / 2)
+            end
+            prompt.border = results.border
 
-                -- Cap over/undersized height
-                height, _ = calc_size_and_spacing(
-                    height,
-                    max_lines,
+            local bs = get_border_size(self)
+
+            -- Cap over/undersized height
+            height, _ = calc_size_and_spacing(
+                height,
+                max_lines,
+                bs,
+                2,
+                3,
+                0
+            )
+
+            -- Height
+            prompt.height = 2
+            results.height = height - prompt.height - (2 * bs)
+            preview.height = results.height - bs
+
+            -- Width
+            local w_space
+
+            preview.width = 0
+            prompt.width = max_columns - 2 * bs
+            if
+                self.previewer
+                and max_columns >= layout_config.preview_cutoff
+            then
+                -- Cap over/undersized width (with preview)
+                width, w_space = calc_size_and_spacing(
+                    max_columns,
+                    max_columns,
                     bs,
                     2,
-                    3,
+                    4,
                     0
                 )
 
-                -- Height
-                prompt.height = 2
-                results.height = height - prompt.height - (2 * bs)
-                preview.height = results.height - bs
-
-                -- Width
-                local w_space
-
+                preview.width = resolve.resolve_width(
+                    if_nil(layout_config.preview_width, 0.6)
+                )(self, width, max_lines)
+                results.width = width - preview.width - w_space
+                prompt.width = results.width
+                results.width = results.width
+            else
+                width, w_space = calc_size_and_spacing(
+                    width,
+                    max_columns,
+                    bs,
+                    1,
+                    2,
+                    0
+                )
                 preview.width = 0
-                prompt.width = max_columns - 2 * bs
-                if
-                    self.previewer
-                    and max_columns >= layout_config.preview_cutoff
-                then
-                    -- Cap over/undersized width (with preview)
-                    width, w_space = calc_size_and_spacing(
-                        max_columns,
-                        max_columns,
-                        bs,
-                        2,
-                        4,
-                        0
-                    )
-
-                    preview.width = resolve.resolve_width(
-                        if_nil(layout_config.preview_width, 0.6)
-                    )(self, width, max_lines)
-                    results.width = width - preview.width - w_space
-                    prompt.width = results.width
-                    results.width = results.width
-                else
-                    width, w_space = calc_size_and_spacing(
-                        width,
-                        max_columns,
-                        bs,
-                        1,
-                        2,
-                        0
-                    )
-                    preview.width = 0
-                    results.width = width - preview.width - w_space
-                    prompt.width = results.width
-                end
-
-                -- Line
-                if layout_config.prompt_position == "top" then
-                    prompt.line = max_lines - results.height - (1 + bs) + 1
-                    results.line = prompt.line + 1
-                    preview.line = prompt.line + 1
-                    if results.border == true then
-                        results.border = { 0, 1, 1, 1 }
-                    end
-                    if type(results.title) == "string" then
-                        results.title = { { pos = "S", text = results.title } }
-                    end
-                elseif layout_config.prompt_position == "bottom" then
-                    results.line = max_lines - results.height - (1 + bs) + 1
-                    results.line = prompt.line + 1
-                    preview.line = results.line
-                    prompt.line = max_lines - bs
-                    if type(prompt.title) == "string" then
-                        prompt.title = { { pos = "S", text = prompt.title } }
-                    end
-                    if results.border == true then
-                        results.border = { 1, 1, 0, 1 }
-                    end
-                else
-                    error(
-                        "Unknown prompt_position: "
-                            .. tostring(self.window.prompt_position)
-                            .. "\n"
-                            .. vim.inspect(layout_config)
-                    )
-                end
-                local width_padding = math.floor((max_columns - width) / 2)
-
-                -- Col
-                prompt.col = 0 -- centered
-                if layout_config.mirror and preview.width > 0 then
-                    results.col = preview.width + (3 * bs)
-                    preview.col = bs + 1
-                    prompt.col = results.col
-                else
-                    preview.col = prompt.width + width_padding + bs + 1
-
-                    results.col = bs + 1
-                    prompt.col = preview.col + preview.width + bs
-                end
-
-                if tbln then
-                    -- prompt.line = prompt.line + 1
-                    results.line = results.line + 1
-                    preview.line = preview.line
-                    prompt.line = prompt.line
-                else
-                    results.line = results.line + 1
-                end
-                prompt.line = prompt.line + 1
-                results.line = results.line + 1
-                preview.col = preview.col + 1
-                preview.height = preview.height - 2
-                prompt.col = 2
-                results.height = results.height - 1
-                preview.height = prompt.height + results.height
-                results.col = 2
-
-                return {
-                    preview = self.previewer and preview.width > 0 and preview,
-                    prompt = prompt,
-                    results = results,
-                }
+                results.width = width - preview.width - w_space
+                prompt.width = results.width
             end
+
+            -- Line
+            if layout_config.prompt_position == "top" then
+                prompt.line = max_lines - results.height - (1 + bs) + 1
+                results.line = prompt.line + 1
+                preview.line = prompt.line + 1
+                if results.border == true then
+                    results.border = { 0, 1, 1, 1 }
+                end
+                if type(results.title) == "string" then
+                    results.title = { { pos = "S", text = results.title } }
+                end
+            elseif layout_config.prompt_position == "bottom" then
+                results.line = max_lines - results.height - (1 + bs) + 1
+                results.line = prompt.line + 1
+                preview.line = results.line
+                prompt.line = max_lines - bs
+                if type(prompt.title) == "string" then
+                    prompt.title = { { pos = "S", text = prompt.title } }
+                end
+                if results.border == true then
+                    results.border = { 1, 1, 0, 1 }
+                end
+            else
+                error(
+                    "Unknown prompt_position: "
+                    .. tostring(self.window.prompt_position)
+                    .. "\n"
+                    .. vim.inspect(layout_config)
+                )
+            end
+            local width_padding = math.floor((max_columns - width) / 2)
+
+            -- Col
+            prompt.col = 0 -- centered
+            if layout_config.mirror and preview.width > 0 then
+                results.col = preview.width + (3 * bs)
+                preview.col = bs + 1
+                prompt.col = results.col
+            else
+                preview.col = prompt.width + width_padding + bs + 1
+
+                results.col = bs + 1
+                prompt.col = preview.col + preview.width + bs
+            end
+
+            if tbln then
+                -- prompt.line = prompt.line + 1
+                results.line = results.line + 1
+                preview.line = preview.line
+                prompt.line = prompt.line
+            else
+                results.line = results.line + 1
+            end
+            prompt.line = prompt.line + 1
+            results.line = results.line + 1
+            preview.col = preview.col + 1
+            preview.height = preview.height - 2
+            prompt.col = 2
+            results.height = results.height - 1
+            preview.height = prompt.height + results.height
+            results.col = 2
+
+            return {
+                preview = self.previewer and preview.width > 0 and preview,
+                prompt = prompt,
+                results = results,
+            }
+        end
         local _bad = { ".*%.md" } -- Put all filetypes that slow you down in this array
         local bad_files = function(filepath)
             for _, v in ipairs(_bad) do
@@ -314,7 +314,7 @@ tele_mod.configs = {
                 opts.use_ft_detect = true
             end
             opts.use_ft_detect = opts.use_ft_detect == false and false
-                or bad_files(filepath)
+            or bad_files(filepath)
             previewers.buffer_previewer_maker(filepath, bufnr, opts)
         end
 
@@ -435,9 +435,9 @@ tele_mod.keybindings = function()
             e = { "<cmd>Telescope emoji<cr>", "Emoji" },
         },
     }, {
-        prefix = "<leader>",
-        mode = "n",
-    })
+            prefix = "<leader>",
+            mode = "n",
+        })
 end
 
 tele_mod.api = {
