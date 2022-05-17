0871a94d564858d63fbfdb11edef2f9806c13b2b
max397574 <max.heinzer397@gmx.ch>
2022-05-17 18:43:19 +0200

    fix(lightspeed): mappings

--- lua/omega/modules/utils/lightspeed.lua
+++ lua/omega/modules/utils/lightspeed.lua
@@ -5,20 +5,27 @@ lightspeed.plugins = {
     ["lightspeed.nvim"] = {
         "ggandor/lightspeed.nvim",
         keys = { "S", "s", "f", "F", "t", "T" },
+        setup = function()
+            vim.g.lightspeed_no_default_keymaps = true
+            vim.keymap.set("n", "s", "<plug>Lightspeed_s")
+            vim.keymap.set("n", "S", "<plug>Lightspeed_S")
+            vim.keymap.set("n", "f", "<plug>Lightspeed_f")
+            vim.keymap.set("n", "F", "<plug>Lightspeed_F")
+            vim.keymap.set("n", "t", "<plug>Lightspeed_t")
+            vim.keymap.set("n", "T", "<plug>Lightspeed_T")
+        end,
     },
 }
 
-lightspeed.configs={
-    ["lightspeed.nvim"]=function()
-        vim.cmd([[
-            sunmap f
-            sunmap F
-            sunmap s
-            sunmap S
-            sunmap t
-            sunmap T
-        ]])
-    end
+lightspeed.configs = {
+    ["lightspeed.nvim"] = function()
+        vim.keymap.set("n", "s", "<plug>Lightspeed_s")
+        vim.keymap.set("n", "S", "<plug>Lightspeed_S")
+        vim.keymap.set("n", "f", "<plug>Lightspeed_f")
+        vim.keymap.set("n", "F", "<plug>Lightspeed_F")
+        vim.keymap.set("n", "t", "<plug>Lightspeed_t")
+        vim.keymap.set("n", "T", "<plug>Lightspeed_T")
+    end,
 }
 
 return lightspeed
