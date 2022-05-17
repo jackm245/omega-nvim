33804c5cc4a8c3d2489fce4f05676185c639785b
max397574 <max.heinzer397@gmx.ch>
2022-05-17 17:41:53 +0200

    feat(misc): added gitsigns

---
+++ lua/omega/modules/misc/gitsigns.lua
@@ -0,0 +1,97 @@
+---@type OmegaModule
+local gitsigns = {}
+
+gitsigns.plugins = {
+    ["gitsigns.nvim"] = {
+        "lewis6991/gitsigns.nvim",
+        opt = true,
+        setup = function()
+            vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter" }, {
+                callback = function()
+                    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+                    if lines ~= { "" } then
+                        if
+                            vim.fn.system(
+                                "git ls-files --error-unmatch "
+                                    .. vim.fn.expand("%")
+                                    .. ""
+                            )
+                            == vim.fn.expand("%") .. "\n"
+                        then
+                            require("packer").loader("gitsigns.nvim")
+                        end
+                    end
+                end,
+            })
+        end,
+    },
+}
+
+gitsigns.configs = {
+    ["gitsigns.nvim"] = function()
+        require("gitsigns").setup({
+            -- current_line_blame = true,
+            signs = {
+                add = {
+                    hl = "GitSignsAdd",
+                    text = "▍",
+                    numhl = "GitSignsAddNr",
+                    linehl = "GitSignsAddLn",
+                },
+                change = {
+                    hl = "GitSignsChange",
+                    text = "▍",
+                    numhl = "GitSignsChangeNr",
+                    linehl = "GitSignsChangeLn",
+                },
+                delete = {
+                    hl = "GitSignsDelete",
+                    text = "▸",
+                    numhl = "GitSignsDeleteNr",
+                    linehl = "GitSignsDeleteLn",
+                },
+                topdelete = {
+                    hl = "GitSignsDelete",
+                    text = "▾",
+                    numhl = "GitSignsDeleteNr",
+                    linehl = "GitSignsDeleteLn",
+                },
+                changedelete = {
+                    hl = "GitSignsChange",
+                    text = "▍",
+                    numhl = "GitSignsChangeNr",
+                    linehl = "GitSignsChangeLn",
+                },
+            },
+            keymaps = {
+                -- Default keymap options
+                noremap = true,
+                buffer = true,
+                ["n ]c"] = {
+                    expr = true,
+                    "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
+                },
+                ["n [c"] = {
+                    expr = true,
+                    "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
+                },
+                ["n <leader>ghs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+                ["n <leader>ghu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+                ["n <leader>ghr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+                ["n <leader>ghR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+                ["n <leader>ghp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+                ["n <leader>ghb"] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+                -- Text objects
+                ["o ih"] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+                ["x ih"] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+            },
+        })
+        vim.cmd([[
+            highlight GitSignsAdd guifg=#9ece6a
+            highlight GitSignsChange guifg=#e0af68
+            highlight GitSignsDelete guifg=#db4b4b
+        ]])
+    end,
+}
+
+return gitsigns
