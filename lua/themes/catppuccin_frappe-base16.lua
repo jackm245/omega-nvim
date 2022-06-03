28172a811606601921cee63da435066b592959a0
max397574 <max.heinzer397@gmx.ch>
2022-06-03 08:19:13 +0200

    feat(themes): added catppuccin

---
+++ lua/themes/catppuccin_frappe-base16.lua
@@ -0,0 +1,19 @@
+return {
+    base00 = "#303446", -- base
+    base01 = "#414559", -- surface0
+    base02 = "#51576d", -- surface1
+    base03 = "#626880", -- surface2
+    base04 = "#babbf1", -- lavender
+    -- base04 = "#c6d0f5", -- text
+    base05 = "#c6d0f5", -- text
+    base06 = "#c6d0f5", -- text
+    base07 = "#c6d0f5", -- text
+    base08 = "#eebebe", -- flamingo
+    base09 = "#ef9f76", -- peach
+    base0A = "#f4b8e4", -- pink
+    base0B = "#a6d189", -- green
+    base0C = "#f4b8e4", -- pink
+    base0D = "#8caaee", -- blue
+    base0E = "#ca9ee6", -- pink
+    base0F = "#8caaee", -- blue
+}
