d3a98b53dbb3606794354f43c49a996f369098f2
max397574 <max.heinzer397@gmx.ch>
2022-06-25 15:34:56 +0200

    feat: write init.lua in norg

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
+require("impatient").enable_profile()
 vim.cmd([[luafile ~/neovim_plugins/profiler.nvim/lua/profiler.lua]])
 vim.opt.runtimepath:append(vim.fn.stdpath("data"))
-
 require("omega.core")
