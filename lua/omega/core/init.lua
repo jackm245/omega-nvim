827e2f7fb7c84de3f2a3b6ebc215cb42b7637e1e
max397574 <max.heinzer397@gmx.ch>
2022-06-25 16:03:18 +0200

    feat(core): write core/init.lua in norg

--- lua/omega/core/init.lua
+++ lua/omega/core/init.lua
@@ -1,24 +1,22 @@
-local utils = require("omega.utils")
--- set early so mappings are defined with it
 vim.g.mapleader = " "
 
--- create `omega` gloabel variable
 require("omega.core.omega_global")
--- add config to `omega` global
 require("omega.core.config").load()
 
 local modules = require("omega.core.modules")
 
 modules.bootstrap_packer()
 
+local utils = require("omega.utils")
 utils.bootstrap_plugins()
 
 require("omega.core.settings")
 require("omega.core.autocommands")
 require("omega.core.commands")
-
 require("omega.core.ui")
+
 modules.setup()
 modules.load()
+
 require("omega.core.mappings")
 require("omega.utils.types")
