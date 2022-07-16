8b4ab41a67791c211f985f2ec0260b23c4967aa9
max397574 <max.heinzer397@gmx.ch>
2022-07-16 11:53:52 +0200

    feat(swift): add xbase config

--- lua/omega/modules/lsp/swift.lua
+++ lua/omega/modules/lsp/swift.lua
@@ -3,13 +3,19 @@ local swift_mod = {}
 swift_mod.plugins = {
     ["xbase"] = {
         "tami5/xbase",
-        ft = "switft",
-        run = "make install",
+        ft = "swift",
         requires = {
             "nvim-lua/plenary.nvim",
+            "neovim/nvim-lspconfig",
             "nvim-telescope/telescope.nvim",
         },
     },
+}
+
+swift_mod.configs = {
+    ["xbase"] = function()
+        require("xbase").setup()
+    end,
 }
 
 return swift_mod
