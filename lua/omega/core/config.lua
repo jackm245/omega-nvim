b3efcd55685526ff2ae7a3f911d274defd6b5fe7
max397574 <max.heinzer397@gmx.ch>
2022-07-18 14:47:08 +0200

    feat(config): use onedark

--- lua/omega/core/config.lua
+++ lua/omega/core/config.lua
@@ -4,7 +4,7 @@ function config.load()
     omega.config = {
         -- "round_colored_bg"|"round_dark_bg"|"round_blended"
         statusline = "round_blended",
-        colorscheme = "catppuccin_frappe",
+        colorscheme = "onedark",
         light_colorscheme = "everforest",
         use_impatient = true,
         --- string "float_all_borders"|"custom_bottom_no_borders"
