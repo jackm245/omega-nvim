5af2fd7acf4e746eecf10608750245006a1e8468
max397574 <max.heinzer397@gmx.ch>
2022-07-18 14:52:15 +0200

    feat(misc): add hologram.nvim

---
+++ lua/omega/modules/misc/holo.lua
@@ -0,0 +1,16 @@
+local holo = {}
+
+holo.plugins={
+    ["hologram.nvim"]={
+        "edluffy/hologram.nvim",
+        module="hologram",
+    }
+}
+
+holo.configs={
+    ["hologram.nvim"]=function()
+        require"hologram".setup()
+    end
+}
+
+return holo
