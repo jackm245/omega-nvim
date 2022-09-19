5df04a882c3795c57881ab4de7dc1f0f52637089
Max <max.heinzer397@gmx.ch>
2022-09-19 09:14:14 +0200

    improve autocmd for insert mode maps

--- lua/omega/core/autocommands.lua
+++ lua/omega/core/autocommands.lua
@@ -236,7 +236,7 @@ aucmd("InsertCharPre", {
     callback = function()
         if vim.v.char == " " then
             vim.keymap.set("i", "<tab>", function()
-                pcall(vim.keymap.del,"i", "<tab>")
+                pcall(vim.keymap.del, "i", "<tab>")
                 vim.api.nvim_input("<bs>")
                 vim.defer_fn(function()
                     require("luasnip").expand_or_jump()
@@ -246,18 +246,21 @@ aucmd("InsertCharPre", {
                 silent = true,
             })
             vim.defer_fn(function()
-                pcall(vim.keymap.del,"i", "<tab>")
+                pcall(vim.keymap.del, "i", "<tab>")
             end, vim.o.timeoutlen)
             vim.keymap.set("i", " ", function()
-                pcall(vim.keymap.del,"i", " ")
+                pcall(vim.keymap.del, "i", " ")
                 vim.api.nvim_input("<bs><right>")
             end, {
                 noremap = true,
                 silent = true,
             })
             vim.defer_fn(function()
-                pcall(vim.keymap.del,"i", " ")
+                pcall(vim.keymap.del, "i", " ")
             end, vim.o.timeoutlen)
+        else
+            pcall(vim.keymap.del, "i", " ")
+            pcall(vim.keymap.del, "i", "<tab>")
         end
     end,
 })
