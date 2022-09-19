efc44f77e7120bce4ffb4911c0c5faefbc639fbc
Max <max.heinzer397@gmx.ch>
2022-09-19 10:46:01 +0200

    try to fix some highlight issues

---
+++ lua/omega/colors/custom/everforest_light.lua
@@ -0,0 +1,11 @@
+-- local colors = require("omega.colors").get()
+--
+vim.opt.background = "light"
+return {
+    --     ["Visual"] = { bg = colors.light_grey },
+    --     ["CursorLine"] = { bg = colors.light_grey },
+    --     ["FloatBorder"] = { fg = colors.light_grey },
+    --     ["CmpCompletionWindowBorder"] = { fg = colors.light_grey },
+    --     ["CmpDocumentationWindowBorder"] = { fg = colors.light_grey },
+    --     ["WinSeparator"] = { fg = colors.light_grey },
+}
